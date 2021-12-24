0193c0ee407d362c645d2397d4b9f0da6fef5711
jalaldgr <jalaldgr@gmail.com>
2021-12-24 13:28:56 +0330

    Step 1.05: change splash screen add animation

--- app/src/main/java/ir/alvandkalallux/shop/SplashActivity.java
+++ app/src/main/java/ir/alvandkalallux/shop/SplashActivity.java
@@ -1,17 +1,32 @@
 package ir.alvandkalallux.shop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class SplashActivity extends AppCompatActivity {
-
+    ImageView logo ;
+    LinearLayoutCompat titleLaout;
+    Animation topanim,btmanim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        logo = findViewById(R.id.SplashImgView);
+        titleLaout = findViewById(R.id.SplashTitleLayout);
+
+        topanim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        btmanim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        //set animation to image and text
+        logo.setAnimation(topanim);
+        titleLaout.setAnimation(btmanim);
 
 
         new Handler().postDelayed(new Runnable() {
